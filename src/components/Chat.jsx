1153a802edbce5e6d6237c0d7ab35e87b5c46c81
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2023-01-25 15:59:51 +0530

    feat: chat added

---
+++ src/components/Chat.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Cam from '../images/cam.png';
+import Add from '../images/add.png';
+import More from '../images/more.png';
+
+const Chat = () => {
+  return (
+    <div className='chat'>
+        <div className="chatInfo">
+            <span>Isha</span>
+            <div className="chatIcons">
+                <img src={Cam} alt="" />
+                <img src={Add} alt="" />
+                <img src={More} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Chat
