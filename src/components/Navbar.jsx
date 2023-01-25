c2e6aaa8d395d2394dd8d74eae5a0bef9c75daa2
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2023-01-25 15:13:20 +0530

    feat : navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <span className='logo'>Isha Chat</span>
+        <div className='user'>
+            <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <span>Isha</span>
+            <button>Logout</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
